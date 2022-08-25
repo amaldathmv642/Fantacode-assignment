2ade1b288ded06a190db85140812f0e9b1e61941
amaldathmv642 <101540213+amaldathmv642@users.noreply.github.com>
2022-08-25 11:55:00 +0530

    Add files via upload

---
+++ MyApp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+    using System;  
+      
+    namespace DateTimeFormatInCSharpSample  
+    {  
+        class Program  
+        {  
+            static void Main(string[] args)  
+            {  
+                 
+                DateTime aDate = DateTime.Now;  
+      Console.WriteLine(aDate.ToString("dddd MMMM"));  
+                Console.ReadKey();  
+            }  
+        }  
+    }  
